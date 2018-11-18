4b6a1bc71e64159f16cb36c0f69f6d085592817a
renxy <renxy@hzanchu.com>
2018-11-18 23:32:51 +0800

    添加user

---
+++ app/Http/Controllers/Home/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+
+class User extends Controller
+{
+    public function index()
+    {
+        var_dump('成功');
+    }
+}
