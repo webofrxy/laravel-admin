64f7174ffce17800d281fb623f66905182bcfb49
renxy <913686609@qq.com>
2018-11-22 17:16:06 +0800

    添加base model 和 article model

---
+++ app/Models/Base.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Baijunyao\LaravelModel\Models\BaseModel;
+
+class Base extends BaseModel
+{
+    //软删除
+    use SoftDeletes;
+}
